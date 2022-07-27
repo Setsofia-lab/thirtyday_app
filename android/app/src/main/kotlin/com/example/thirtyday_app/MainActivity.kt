b59f849945c58790fab48dd175eef018b0e19af7
Setsofia-lab <setsofiaeli@gmail.com>
2022-07-27 10:29:13 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/thirtyday_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thirtyday_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/day1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(children: [
+       Image.asset('images/day1img.png')
+        ],),
+    );
+  }
+}
